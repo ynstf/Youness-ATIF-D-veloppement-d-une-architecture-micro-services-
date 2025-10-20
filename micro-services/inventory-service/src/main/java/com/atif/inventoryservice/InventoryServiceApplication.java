1b22f38d51b56eee3f4cd08f32fc89feeb452a5a
YONES <107154559+ynstf@users.noreply.github.com>
2025-10-20 18:16:58 +0100

    add products

--- micro-services/inventory-service/src/main/java/com/atif/inventoryservice/InventoryServiceApplication.java
+++ micro-services/inventory-service/src/main/java/com/atif/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,13 @@
 package com.atif.inventoryservice;
 
+import com.atif.inventoryservice.entities.Product;
+import com.atif.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +15,31 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Computer")
+                    .price(3200)
+                    .quantity(11)
+                    .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Printer")
+                    .price(1299)
+                    .quantity(10)
+                    .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Smart Phone")
+                    .price(5400)
+                    .quantity(8)
+                    .build());
 
+            productRepository.findAll().forEach(p->{
+                System.out.println(p.toString());
+            });
+        };
+    }
 }
