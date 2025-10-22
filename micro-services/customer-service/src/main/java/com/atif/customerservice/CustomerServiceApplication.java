b3545bb7d6df5095b9b92f8984f0d0884bdb063d
YONES <107154559+ynstf@users.noreply.github.com>
2025-10-22 14:47:12 +0100

    add config repo

--- micro-services/customer-service/src/main/java/com/atif/customerservice/CustomerServiceApplication.java
+++ micro-services/customer-service/src/main/java/com/atif/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.atif.customerservice;
 
+import com.atif.customerservice.config.CustomerConfigParams;
 import com.atif.customerservice.entities.Customer;
 import com.atif.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
---
+++ micro-services/customer-service/src/main/java/com/atif/customerservice/config/CustomerConfigParams.java
@@ -0,0 +1,8 @@
+package com.atif.customerservice.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "customer.params")
+public record CustomerConfigParams(int x, int y) {
+}
